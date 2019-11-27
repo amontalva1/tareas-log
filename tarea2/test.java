611fe784b0d44f42c677403b6137b3aab05dbbab
Antonio Montalva <a.montalva7@gmail.com>
2019-11-27 20:22:11 -0300

    se incluye test de grafo de 8 nodos

--- tarea2/test.java
+++ tarea2/test.java
@@ -7,13 +7,24 @@ public class test{
 
 
     public static void main(String[] args) {
-        int[][] g = {{0,1},{0,0}};
+        //int[][] g = {{0,1},{0,0}};
+        
+        int[][] g = {
+        		{0,5,7,3,0,0,0,0},
+        		{5,0,0,0,2,10,0,0},
+        		{7,0,0,0,0,0,1,0},
+        		{3,0,0,0,0,0,0,11},
+        		{0,2,0,0,0,0,0,9},
+        		{0,10,0,0,0,0,0,4},
+        		{0,0,1,0,0,0,0,6},
+        		{0,0,0,6,11,9,4,0}
+        };
 
         ArrayList<Edge> edges = new ArrayList<Edge>();
 
         //Se guarda en edges todas las aristas dada la matriz de adyacencia
-        for(int i=0; i<2; i++){
-            for(int j=0; j<2; j++){
+        for(int i=0; i<g.length; i++){
+            for(int j=0; j<g.length; j++){
                 if(g[i][j]>0){
                     edges.add(new Edge(i,j, g[i][j]));
                 }
@@ -37,7 +48,7 @@ public class test{
         while(uf.numberOfComps()>k){
             Edge e = edges.get(i);
             
-            //System.out.println(e.info()); print para debugear de panita jeje
+            System.out.println(e.info()); //print para debugear de panita jeje
 
             //Si la union fue exitosa, entonces se añade ese Edge al MSC
             if(uf.union(e.getU(), e.getV())){
