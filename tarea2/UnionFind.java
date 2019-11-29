f52611169e482c89f0a912fa83ec8d659f5efed1
Antonio Montalva <a.montalva7@gmail.com>
2019-11-28 21:25:28 -0300

    se incluyen metodos sin optimizaciones

--- tarea2/UnionFind.java
+++ tarea2/UnionFind.java
@@ -14,6 +14,7 @@ public class UnionFind{
         }
     }
 
+    //find que usa compresion de caminos
     public int find(int x){
         int aux = ids[x];
         //busca el root de la componente x
@@ -30,6 +31,18 @@ public class UnionFind{
         return aux;
     }
 
+    //find que no usa compresion de caminos
+    public int findWOcompression(int x){
+        int aux = ids[x];
+        // busca el root de la componente x
+        while (ids[aux] != aux) {
+            aux = ids[aux];
+        }
+        // retorna el root de x
+        return aux;
+    }
+
+    //union con compresión de caminos y con union por rangos
     public boolean union(int x, int y){
         //busca la raices de cada componente x e y
         int rootX = find(x);
@@ -48,6 +61,68 @@ public class UnionFind{
                 ids[rootX] = rootY;
                 sizes[rootY] += sizes[rootX];
             }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    //union sin compresion de caminos y con union por rangos
+    public boolean union2(int x, int y){
+        // busca la raices de cada componente x e y
+        int rootX = findWOcompression(x);
+        int rootY = findWOcompression(y);
+        // si no son de la misma componente x e y, procede a unirlos
+        if (rootX != rootY) {
+            nOfComps--; // si se logra unir dos componentes se resta una a la cantidad de componentes
+
+            // si x es mas grande que y, x se vuelve el padre de y
+            if (sizes[x] >= sizes[y]) {
+                ids[rootY] = rootX;
+                sizes[rootX] += sizes[rootY];
+            }
+            // de lo contrario y se vuelve el padre de x
+            else {
+                ids[rootX] = rootY;
+                sizes[rootY] += sizes[rootX];
+            }
+
+            return true;
+        }
+
+        return false;        
+    }
+
+    //union con compresion de caminos y sin union por rangos
+    public boolean union3(int x, int y){
+        // busca la raices de cada componente x e y
+        int rootX = find(x);
+        int rootY = find(y);
+        // si no son de la misma componente x e y, procede a unirlos
+        if (rootX != rootY) {
+            nOfComps--; // si se logra unir dos componentes se resta una a la cantidad de componentes
+
+            ids[rootY] = rootX;
+            sizes[rootX] += sizes[rootY];
+
+            return true;
+        }
+
+        return false;
+    }
+
+    //union sin compresion de sin union por rangos
+    public boolean union4(int x, int y) {
+        // busca la raices de cada componente x e y
+        int rootX = findWOcompression(x);
+        int rootY = findWOcompression(y);
+        // si no son de la misma componente x e y, procede a unirlos
+        if (rootX != rootY) {
+            nOfComps--; // si se logra unir dos componentes se resta una a la cantidad de componentes
+
+            ids[rootY] = rootX;
+            sizes[rootX] += sizes[rootY];
 
             return true;
         }
